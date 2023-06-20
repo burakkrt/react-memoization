2aa653d283c1f4382f8467eef386e9cf3a64bc93
BurakKurt <krtburak@outlook.com>
2023-06-20 14:02:41 +0300

    project sekeleton created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className="text-danger">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</h1>
+    </div>
+  );
+}
+
+export default App;
